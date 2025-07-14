f3b1e6538347dd77609da792482611178a341860
khushishingala <shingalkhushi22@gmail.com>
2025-07-14 11:36:43 +0530

    assignment-1

--- assignment-1.js
+++ assignment-1.js
@@ -1,9 +1,33 @@
-const n = 7
+////1. Check Even or Odd
+// const n = 7
 
-if(n%2)
+// if(n%2)
+// {
+//     console.log("Odd Number")
+// }else
+// {
+//     console.log("Even Number")
+// }
+
+// 2. Find the Maximum of Two Numbers
+
+// let a = 5;
+// let b = 9;
+
+// if (a > b) {
+//   console.log(a);  
+// } else {
+//   console.log(b);
+// }
+
+//3. Check Leap Year
+
+let year = 2020; 
+
+
+if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) 
 {
-    console.log("Odd Number")
-}else
-{
-    console.log("Even Number")
+    console.log(year + " is a Leap Year.");
+} else {
+    console.log(year + " is not a Leap Year.");
 }
