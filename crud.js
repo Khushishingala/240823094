aa6f2956c3ddb9fcba1f68b4bd37291ebfeb4169
khushishingala <shingalakhushi22@gmail.com>
2025-08-07 08:50:36 +0530

    crud

---
+++ crud.js
@@ -0,0 +1,30 @@
+const express=require("express");
+const app=express();
+const PORT=80;
+
+app.use(express.json());
+app.get("/",(req,res)=>{
+    res.send("Hello,BCA students!\n");
+});
+
+app.get("/show/:id",(req,res)=>{
+    res.send("hello,student\nID:${req.params.id}");
+});
+
+app.post("/store",(req,res)=>{
+    res.send("successfully stored record\n ${req.body.name},${req.body.city}");
+
+});
+
+app.put("/update/:id",(req,res)=>{
+    res.send(
+        "successfully updated record !\n ID:${req.params.id},Name:${req.body.name},${req.body,city}");
+});
+
+app.delete("/delete/:id",(req,res)=>{
+    res.send("successfully deleted record with ID:${req.params.id}");
+});
+
+app.listen(PORT,()=>{
+    console.log(`server is running at http://localhost:${PORT}/`);
+});
